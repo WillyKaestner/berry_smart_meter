798b721be7fcaf516c45475f33ac63ffeb77032b
willy kaestner <willy.kaestner1@gmail.com>
2023-08-17 23:22:29 +0100

    (feat): downgraded pydantic to 1.10.8

--- config.py
+++ config.py
@@ -3,16 +3,18 @@ import pydantic as pyd
 
 
 class ConfigMeter(pyd.BaseModel):
-    run_type: str = pyd.Field(validation_alias=pyd.AliasChoices('RUN_TYPE', 'RUN TYPE', 'run type'))
-    meter_uuid: pyd.UUID4 = pyd.Field(validation_alias=pyd.AliasChoices('METER_UUID', 'METER UUID', 'meter uuid'))
+    # run_type: str = pyd.Field(validation_alias=pyd.AliasChoices('RUN_TYPE', 'RUN TYPE', 'run type'))
+    run_type: str
+    meter_uuid: pyd.UUID4
+    # meter_uuid: pyd.UUID4 = pyd.Field(validation_alias=pyd.AliasChoices('METER_UUID', 'METER UUID', 'meter uuid'))
 
-    @pyd.field_validator('run_type')
-    @classmethod
-    def check_run_type(cls, v: str) -> str:
-        supported_run_types = ("dry run", "run")
-        if v.lower() not in supported_run_types:
-            raise ValueError(f"run type:'{v}' not supported. Use of of the following: {supported_run_types}")
-        return v.lower()
+    # @pyd.field_validator('run_type')
+    # @classmethod
+    # def check_run_type(cls, v: str) -> str:
+    #     supported_run_types = ("dry run", "run")
+    #     if v.lower() not in supported_run_types:
+    #         raise ValueError(f"run type:'{v}' not supported. Use of of the following: {supported_run_types}")
+    #     return v.lower()
 
 
 def open_config_meter(file_path):
