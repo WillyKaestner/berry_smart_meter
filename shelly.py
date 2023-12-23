a1113ba31351537f118ca803924d667153696916
willy kaestner <willy.kaestner1@gmail.com>
2023-12-23 23:12:49 +0000

    changed limit to 4000 watt

--- shelly.py
+++ shelly.py
@@ -7,7 +7,7 @@ IP_ADDRESS_PLUG_1 = "192.168.1.91"
 
 
 def control_plugs(energy_data):
-    if energy_data.real_power > 200000:
+    if energy_data.real_power > 400000:
         turn_shelly_off(IP_ADDRESS_PLUG_1)
     else:
         turn_shelly_on(IP_ADDRESS_PLUG_1)
@@ -17,7 +17,7 @@ def turn_shelly_on(ip_address):
     url = f'http://{ip_address}/relay/0?turn=on'
     try:
         response = requests.get(url)
-        logger.info(f"Shelly plug turned on. Response: {response}")
+        logger.info(f"Shelly plug with IP {ip_address} turned on. Response: {response}")
         return response
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -29,7 +29,7 @@ def turn_shelly_off(ip_address):
     url = f'http://{ip_address}/relay/0?turn=off'
     try:
         response = requests.get(url)
-        logger.info(f"Shelly plug turned off. Response: {response}")
+        logger.info(f"Shelly plug with IP {ip_address} turned off. Response: {response}")
         return response
     except Exception as e:
         print(f"An error occurred: {e}")
